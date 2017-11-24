2cf67f929d1439eea5d40d82b1d0cf0234194476
Ziyang tan <ziyang0621@gmail.com>
2017-11-23 16:53:37 -0800

    minor update

---
+++ playground.js
@@ -0,0 +1,18 @@
+// write a function to retrieve a blob of json
+// make an ajax request! Use the 'fetch' function.
+// https://rallycoding.herokuapp.com/api/music_albums
+
+// function fetchAlbums() {
+//   fetch('https://rallycoding.herokuapp.com/api/music_albums')
+//     .then(res => res.json())
+//     .then(json => console.log(json));
+// }
+
+const fetchAlbums = async () => {
+  const res =  await fetch('https://rallycoding.herokuapp.com/api/music_albums')
+  const json = await res.json();
+
+  console.log(json);
+}
+
+fetchAlbums();
